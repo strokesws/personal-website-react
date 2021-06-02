faf44b196e1be2a99281538b7f63cbf694af3a1d
Matheus Sartoretto <strokesws@gmail.com>
2021-06-02 21:33:25 +0100

    add header menu options

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,12 +1,21 @@
 import React, { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function HeaderMenu(): ReactElement {
-  const menuItems = [{ name: 'a' }];
+  const menuItems = [
+    { id: 1, path: '/', name: 'Home' },
+    { id: 2, path: '/about', name: 'About' },
+    { id: 3, path: '/my-timeline', name: 'My Timeline' },
+  ];
 
   return (
     <nav className="p-4 flex justify-end">
       {menuItems.map((item) => {
-        return <a href="/home">{item.name}</a>;
+        return (
+          <Link className="p-3" to="{item.path}" key="{item.id}">
+            {item.name}
+          </Link>
+        );
       })}
     </nav>
   );

e3d44a3b1c4169260c6e3b6c246078ab95b0c2f2
Matheus Sartoretto <strokesws@gmail.com>
2021-06-02 21:15:10 +0100

    mv Home view to views folder

---
+++ src/views/Home.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from 'react';
+import logo from 'logo.svg';
+import './Home.css';
+
+export default function Home(): ReactElement {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}

8395e9b9dc7af241f79fcfcde486f28ba1c486ae
Matheus Sartoretto <strokesws@gmail.com>
2021-06-02 19:56:31 +0100

    eslint-disable craco config

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
   style: {
     postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
+      plugins: [tailwindcss, autoprefixer],
     },
   },
 };
